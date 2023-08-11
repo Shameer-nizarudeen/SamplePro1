fe4b44190e54c8c271e590a650d67c2f4281f9bf
muhammedshameer <muhammedshameer@192.168.20.6>
2023-08-11 10:32:03 +0530

    Its Hello World.

---
+++ MavenPro1/src/main/java/samplepro/Mavenpro1.java
@@ -0,0 +1,15 @@
+package samplepro;
+
+public class Mavenpro1 {
+	public void print()
+	{
+		System.out.print("Helo World");	}
+
+	public static void main(String[] args) 
+	{
+		
+		 Mavenpro1 s=new Mavenpro1();
+		 s.print();
+	}
+
+}
