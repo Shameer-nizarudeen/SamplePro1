5dc34afb38151c025ce9990e96a488267577ffbb
muhammedshameer <muhammedshameer@localhost>
2023-08-21 11:40:25 +0530

    sample pgm

---
+++ MavenPro1/src/main/java/git/ExAdd.java
@@ -0,0 +1,21 @@
+package git;
+
+public class ExAdd {
+	public int add()
+	{
+		int a=10;
+		int b=10;
+		int c=a+b;
+		return c;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ExAdd s=new ExAdd();
+		int x=s.add();
+		System.out.print("The The is Sum Is "+x);
+
+	}
+
+}
+ 
---
+++ MavenPro1/src/main/java/git/ExName1.java
@@ -0,0 +1,14 @@
+package git;
+
+public class ExName1 {
+	public void print()
+	{
+		System.out.print("Hello world");	}
+
+	public static void main(String[] args) {
+	
+ExName1 s=new ExName1();
+s.print();
+	}
+
+}
